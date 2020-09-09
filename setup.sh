fb4cb6f11678e706f36dd1c9b1445b918deb75b6
gbudau <gbudau@student.42.fr>
2020-09-09 20:44:47 +0200

    Created .yaml files for Nginx service and modified Dockerfile of nginx

--- setup.sh
+++ setup.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-echo "metalLB manifest"
+# Start minikube
+#minikube start --driver=virtualbox
+
+# Install metallb
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f srcs/metallb/config.yaml
 
-#echo "dashboard activate:
+# Build images
+eval $(minikube docker-env)
+docker build -t my-nginx srcs/nginx
+
+# Load configs
+kubectl apply -f srcs/nginx/nginx_deployment.yaml
+kubectl apply -f srcs/nginx/nginx_service.yaml
+
+# Activate dashboard
 #minikube dashboard
--- srcs/clean.sh
+++ srcs/clean.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-echo "metalLB manifest delete"
+# Delete the namespace
 kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+
+kubectl delete svc nginx
+kubectl delete deploy nginx-deployment
+
+#minikube stop
+#minikube delete
