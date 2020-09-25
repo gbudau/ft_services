d8e0c0ef9a08a911d6d091bccb37eabf0dde29ab
gbudau <54190400+gbudau@users.noreply.github.com>
2020-09-25 19:56:38 +0200

    Running wp-cli as www user Added condition to check if the persistent volume is empty before initializing Wordpress or MySQL

--- srcs/mysql/startup.sh
+++ srcs/mysql/startup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env ash
 
 DIR="/var/lib/mysql"
-/usr/bin/mysql_install_db --basedir=/usr --user=mysql --datadir="$DIR"
-cat << EOF > tmp.sql
+if [ -z "$(ls -A -- "$DIR")" ]; then
+	/usr/bin/mysql_install_db --basedir=/usr --user=mysql --datadir="$DIR"
+	cat << EOF > tmp.sql
 USE mysql;
 FLUSH PRIVILEGES ;
 GRANT ALL ON *.* TO 'root'@'172.17.0.%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
@@ -11,13 +12,12 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON phpmyadmin.* TO 'pma'@'172.17.0.%' IDENT
 GRANT ALL ON *.* TO dbuser@'172.17.0.%' IDENTIFIED BY '${DATABASE_USER_PASSWORD}' ;
 CREATE DATABASE wordpress;
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'172.17.0.%' IDENTIFIED BY '${WORDPRESS_DATABASE_PASSWORD}';
-FLUSH PRIVILEGES;
 DROP DATABASE IF EXISTS test ;
 FLUSH PRIVILEGES ;
 EOF
-/usr/bin/mysqld --defaults-file=/etc/my.cnf --user=mysql --bootstrap < create_tables.sql
-/usr/bin/mysqld --defaults-file=/etc/my.cnf --user=mysql --bootstrap < tmp.sql
-rm tmp.sql
-rm create_tables.sql
-
+	/usr/bin/mysqld --defaults-file=/etc/my.cnf --user=mysql --bootstrap < create_tables.sql
+	/usr/bin/mysqld --defaults-file=/etc/my.cnf --user=mysql --bootstrap < tmp.sql
+	rm -f tmp.sql
+fi
+rm -f create_tables.sql
 exec /usr/bin/mysqld --user=mysql
--- srcs/wordpress/startup.sh
+++ srcs/wordpress/startup.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env ash
 
 sed -i -e "s/database_name_here/wordpress/" -e "s/username_here/wordpress/" -e "s/password_here/${WORDPRESS_DATABASE_PASSWORD}/" -e "s/localhost/mysql/" /www/wp-config.php
-cp -r wp-content/* www/wp-content
-until wp db check --path="/www" --allow-root; do echo waiting for mysql; sleep 2; done
-wp core install --path=/www --url="$WORDPRESS_URL" --title="$WORDPRESS_SITE_TITLE" --admin_email="$WORDPRESS_ADMIN_EMAIL" --admin_user="$WORDPRESS_ADMIN" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --skip-email --allow-root
-wp user create "$WORDPRESS_EDITOR" "$WORDPRESS_EDITOR_EMAIL" --user_pass="$WORDPRESS_EDITOR_PASSWORD" --role=editor --path=/www --allow-root
-wp user create "$WORDPRESS_AUTHOR" "$WORDPRESS_AUTHOR_EMAIL" --user_pass="$WORDPRESS_AUTHOR_PASSWORD" --role=author --path=/www --allow-root
-wp user create "$WORDPRESS_CONTRIBUTOR" "$WORDPRESS_CONTRIBUTOR_EMAIL" --user_pass="$WORDPRESS_CONTRIBUTOR_PASSWORD" --role=contributor --path=/www --allow-root
+if [ -z "$(ls -A -- "/www/wp-content")" ]; then
+	cp -r wp-content/* www/wp-content
+fi
+until sudo -u www wp db check --path="/www"; do echo waiting for mysql; sleep 2; done
+if ! $(sudo -u www wp core is-installed --path=/www); then
+	sudo -u www wp core install --path=/www --url="$WORDPRESS_URL" --title="$WORDPRESS_SITE_TITLE" --admin_email="$WORDPRESS_ADMIN_EMAIL" --admin_user="$WORDPRESS_ADMIN" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --skip-email
+	sudo -u www wp user create "$WORDPRESS_EDITOR" "$WORDPRESS_EDITOR_EMAIL" --user_pass="$WORDPRESS_EDITOR_PASSWORD" --role=editor --path=/www
+	sudo -u www wp user create "$WORDPRESS_AUTHOR" "$WORDPRESS_AUTHOR_EMAIL" --user_pass="$WORDPRESS_AUTHOR_PASSWORD" --role=author --path=/www
+	sudo -u www wp user create "$WORDPRESS_CONTRIBUTOR" "$WORDPRESS_CONTRIBUTOR_EMAIL" --user_pass="$WORDPRESS_CONTRIBUTOR_PASSWORD" --role=contributor --path=/www
+fi
 chown -R www:www /www
 rm -rf wp-content
 /usr/sbin/php-fpm7
