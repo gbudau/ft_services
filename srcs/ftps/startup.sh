b7f5a05538603f8eaf25179c8f8e88b75e20da55
gbudau <54190400+gbudau@users.noreply.github.com>
2020-10-07 19:46:37 +0200

    Fixed ftps

--- srcs/ftps/startup.sh
+++ srcs/ftps/startup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env ash
 
-echo "$FTPS_USER" >> /etc/vsftpd.allowed_users
 adduser -D -h /vsftpd/"$FTPS_USER" -s /sbin/nologin "$FTPS_USER"
 echo "$FTPS_USER:$FTPS_USER_PASS" | chpasswd
-mkdir -p /vsftpd/"$FTPS_USER"
-chown "$FTPS_USER:$FTPS_USER" /vsftpd/"$FTPS_USER"
-exec /usr/sbin/vsftpd "-opasv_address=$FTPS_EXTERNAL_IP" /etc/vsftpd/vsftpd.conf
+mkdir -p /var/run/vsftpd/empty
+mkdir -p /vsftpd/"$FTPS_USER"/ftp/files
+chown nobody:nogroup /vsftpd/"$FTPS_USER"/ftp
+chmod a-w /vsftpd/"$FTPS_USER"/ftp
+chown "$FTPS_USER:$FTPS_USER" /vsftpd/"$FTPS_USER"/ftp/files
+echo "$FTPS_USER" > /etc/vsftpd.chroot_list
+/usr/sbin/vsftpd "-opasv_address=$FTPS_EXTERNAL_IP" /etc/vsftpd/vsftpd.conf
