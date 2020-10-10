7ace67aeed92d23e7a6d3f05daddba55aea52f30
gbudau <54190400+gbudau@users.noreply.github.com>
2020-10-11 00:02:46 +0200

    Fixed ConfigMap path in influxdb deployment Added a condition to wait until influxd service is started Changed some user environmental variables to secrets

--- srcs/influxdb/startup.sh
+++ srcs/influxdb/startup.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$(ls -A -- /var/lib/influxdb)" ]; then
 	INFLUXDB_HTTP_BIND_ADDRESS=127.0.0.1:8086 INFLUXDB_HTTP_HTTPS_ENABLED=false influxd &
-	influx -host 127.0.0.1 -port 8086 -execute "CREATE DATABASE telegraf"
+	until influx -host 127.0.0.1 -port 8086 -execute "CREATE DATABASE telegraf"; do echo waiting for influxdb; sleep 2; done
 	pkill influxd
 fi
 
